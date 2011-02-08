12f280439f4281ecdc4e4783dd407b4c03159434
Haakon Nilsen <haakon@ii.uib.no>
2011-02-08 20:44:14 +0100

    * If an IP has more than one reverse, just pick one * Don't handle the broadcast address

--- liveips.sh
+++ liveips.sh
@@ -5,15 +5,16 @@ AWK=/usr/bin/awk
 HOST=/usr/bin/host
 CUT=/usr/bin/cut
 SEQ=/usr/bin/seq
+HEAD=/usr/bin/head
 
 REPORTFILE=$1.live
 
 echo -n > $REPORTFILE
 
-for x in $($SEQ 1 255)
+for x in $($SEQ 1 254)
 do
     ip="$1.$x"
     $PING -q -c 1 $ip >/dev/null &&
-    h=$($HOST $ip | $AWK '{print $NF}' | $CUT -d. -f1) &&
+    h=$($HOST $ip | $HEAD -1 | $AWK '{print $NF}' | $CUT -d. -f1) &&
     echo "$ip $h" >> $REPORTFILE
 done
